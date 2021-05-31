058ee4af99292cf6af417bb8648c115539f684dd
badbotinochek <42091757+badbotinochek@users.noreply.github.com>
2021-05-31 21:05:24 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,127 @@
+from flask import Flask, jsonify, request
+import sqlalchemy as db
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
+from Config1 import Config
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+engine = create_engine('sqlite:///db.sqlite')
+
+session = scoped_session(sessionmaker(
+    autocommit=False, autoflush=False, bind=engine))
+
+Base = declarative_base()
+Base.query = session.query_property()
+
+jwt = JWTManager(app)
+
+from mdels import *
+
+Base.metadata.create_all(bind=engine)
+
+
+# Просмотр всех операций
+@app.route('/costs', methods=['GET'])
+@jwt_required()
+def get_costs():
+    costs = Costs.query.all()
+    serialized = []
+    for cost in costs:
+        serialized.append({
+            'id': cost.id,
+            'category': cost.category,
+            'amount': cost.amount,
+            'description': cost.description,
+            'date': cost.date,
+            'type': cost.type
+        })
+    return jsonify(serialized)
+
+# Добавление новой операции
+@app.route('/costs', methods=['POST'])
+@jwt_required()
+def update_costs():
+    new_one = Costs(**request.json)
+    session.add(new_one)
+    session.commit()
+    serialized = {
+        'id': new_one.id,
+        'category': new_one.category,
+        'amount': new_one.amount,
+        'description': new_one.description,
+        'date': new_one.date,
+        'type': new_one.type
+    }
+    return jsonify(serialized)
+
+# Изменение операции
+@app.route('/costs/<int:cost_id>', methods=['PUT'])
+@jwt_required()
+def change_cost(cost_id):
+    item = Costs.query.filter(Costs.id == cost_id).first()
+    params = request.json
+    if not item:
+        return {'message': 'Траты с таким номером не существует'}, 400
+    for key, value in params.items():
+        setattr(item, key, value)
+    session.commit()
+    serialized = {
+        'id': item.id,
+        'category': item.category,
+        'amount': item.amount,
+        'description': item.description,
+        'date': item.date,
+        'type': item.type
+    }
+    return jsonify(serialized)
+
+# Удаление операции
+@app.route('/costs/<int:cost_id>', methods=['DELETE'])
+@jwt_required()
+def delete_cost(cost_id):
+    item = Costs.query.filter(Costs.id == cost_id).first()
+    params = request.json
+    if not item:
+        return {'message': 'Траты с таким номером не существует'}, 400
+    session.delete(item)
+    session.commit()
+    return '', 204
+
+@app.route('/register', methods=['POST'])
+def register():
+    params = request.json
+    user = User(**params)
+    session.add(user)
+    session.commit()
+    token = user.get_token()
+    return {'access_token': token}
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    params = request.json
+    user = User.authenticate(**params)
+    token = user.get_token()
+    return {'access_token': token}
+
+
+
+
+
+@app.teardown_appcontext
+def shutdown_session(exseption=None):
+    session.remove()
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
---
+++ mdels.py
@@ -0,0 +1,45 @@
+from main import db, session, Base
+from flask_jwt_extended import create_access_token
+from datetime import timedelta
+from passlib.hash import  bcrypt
+
+class Costs(Base):
+    __tablename__ = 'costs'
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String(250), nullable=False)
+    amount = db.Column(db.Text(50), nullable=False)
+    description = db.Column(db.String(250), nullable=False)
+    type = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.TEXT(250), nullable=False)
+
+class User(Base):
+    __tablename__ = 'users'
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, **kwargs):
+        self.name = kwargs.get('name')
+        self.email = kwargs.get('email')
+        self.password = bcrypt.hash(kwargs.get('password'))
+
+    def get_token(self, expire_time=24):
+        expire_delta = timedelta(expire_time)
+        token = create_access_token(
+            identity=self.id, expires_delta=expire_delta)
+        return token
+
+    @classmethod
+    def authenticate(cls, email, password):
+        user = cls.query.filter(cls.email == email).one()
+        if not bcrypt.verify(password, user.password):
+            raise Exception('No user with this password')
+        return user
+
+
+
+
+
