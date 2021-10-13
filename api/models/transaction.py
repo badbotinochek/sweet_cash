e22d2484805f976f71ee4e859bc2a78bfc279101
Navy881 <ag881.pst@gmail.com>
2021-10-13 22:43:48 +0300

    Added new route for getting users transactions

--- api/models/transaction.py
+++ api/models/transaction.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 
 from db import db
 
@@ -8,14 +8,16 @@ class Transaction(db.Model):
     __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True)
     type = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
     category = db.Column(db.Integer, nullable=False)
     amount = db.Column(db.Float, nullable=False)
     transaction_date = db.Column(db.DateTime, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     description = db.Column(db.String(250), nullable=False)
 
     def __init__(self, **kwargs):
         self.type = kwargs.get('type')
+        self.user_id = kwargs.get('user_id')
         self.category = kwargs.get('category')
         self.amount = kwargs.get('amount')
         self.transaction_date = kwargs.get('transaction_date')
@@ -23,3 +25,12 @@ class Transaction(db.Model):
 
     def get_id(self):
         return self.id
+
+    @classmethod
+    def get_transactions(cls, user_id, offset=0, limit=100):
+        query = cls.query.filter(cls.user_id == user_id)
+        if limit:
+            query = query.limit(limit)
+        if offset:
+            query = query.offset(limit * offset)
+        return query
--- api/routes/auth.py
+++ api/routes/auth.py
@@ -1,9 +1,12 @@
-from flask import request, jsonify, Blueprint
+
+from flask import jsonify, Blueprint
+from datetime import datetime
 import re
 
 from db import db
 from api.validator import jsonbody
 from api.models.users import User
+from api.models.session import Session
 import api.errors as error
 
 auth_api = Blueprint('login', __name__)
@@ -12,24 +15,22 @@ auth_api = Blueprint('login', __name__)
 @auth_api.route('/api/v1/login', methods=['POST'])
 @jsonbody(email=(str, "required"),
           password=(str, "required"))
-def login():
-    email = request.json.get('email')
-    password = request.json.get('password')
-
+def login(email: str, password: str):
     if not check_email_format(email):
         raise error.APIParamError('Invalid email format')
 
     user = User.get_user(email)
 
     if user is None:
-        user_id, token = create_new_user(name=email, email=email, password=password)
+        user_id = create_new_user(name=email, email=email, password=password)
+        token = new_token(user_id=user_id, login_method='email')
         return jsonify({"access_token": token,
                         "user_id": user_id}), 200
     else:
         if not user.check_password(password):
             raise error.APIAuthError('Wrong password')
         user_id = user.get_id()
-        token = user.get_token()
+        token = new_token(user_id=user_id, login_method='email')
         return jsonify({"access_token": token,
                         "user_id": user_id}), 200
 
@@ -38,10 +39,26 @@ def create_new_user(name: str, email: str, password: str):
     u = User(name=name, email=email, password=password)
     db.session.add(u)
     db.session.commit()
-    return u.get_id(), u.get_token()
+    return u.get_id()
 
 
 def check_email_format(email: str):
     regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
     result = re.fullmatch(regex, email)
     return result
+
+
+def new_token(user_id: str, login_method: str) -> str:
+    session = Session.get(user_id=user_id)
+
+    if session is not None:
+        session.token = session.new_token()
+        session.login_method = login_method
+        session.updated = datetime.now().isoformat()
+    else:
+        session = Session(user_id=user_id, login_method=login_method)
+        db.session.add(session)
+
+    db.session.commit()
+
+    return session.get_token()
