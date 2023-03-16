ded2aeff1db7546421448c053b54bb9c6f7261df
Navy881 <ag881.pst@gmail.com>
2023-03-16 16:29:21 +0300

    freeze SQLAlchemy version

--- sweet_cash/api/routes/views.py
+++ sweet_cash/api/routes/views.py
@@ -1,13 +1,6 @@
 from flask import Blueprint
 import logging
 
-from sweet_cash.api.api import SuccessResponse, jsonbody, features, query_params
-from sweet_cash.api.services.users.register_user import RegisterUser
-from sweet_cash.api.services.users.login_user import LoginUser
-from sweet_cash.api.services.users.get_access_token import GetAccessToken
-from sweet_cash.api.services.users.confirm_user import ConfirmUser
-from sweet_cash.api.services.email_sending.send_email import SendEmail
-
 logger = logging.getLogger(name="views")
 
 views_api = Blueprint('views', __name__)
